ff5530a8de2f04bfffc01836c294f075f99e35f9
daarya <aryad0525@gmail.com>
2025-04-24 15:26:06 +0530

    Guess the number progamm working fine now

---
+++ projects/guess_number_game.py
@@ -0,0 +1,17 @@
+import random 
+
+number = random.randint(1,100)
+while True:
+    try:
+        gnumber = int(input("Guess the number between 1 and 100 : "))
+        if gnumber > number:
+            print("Too high")
+        elif gnumber < number:
+            print("too low")
+        elif gnumber == number:
+            print("Guessed the right number ")
+            break       
+    except ValueError as e:
+        print("Enter the valid number")
+    
+    
